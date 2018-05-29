d9cb2bed7ed9c02cf39bdf92ad17a9bfa3199276
RamirezJosue <xdemobtkx@gmail.com>
2018-05-29 12:17:41 -0500

    Bredcrums

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -10,9 +10,9 @@ const pagesRoutes: Routes = [
         path: '',
         component: PagesComponent,
         children: [
-          { path: 'dashboard', component: DashboardComponent },
-          { path: 'progress', component: ProgressComponent },
-          { path: 'graficas1', component: Graficas1Component },
+          { path: 'dashboard', component: DashboardComponent, data: {titulo: 'Dashboard'} },
+          { path: 'progress', component: ProgressComponent, data: {titulo: 'Progress'} },
+          { path: 'graficas1', component: Graficas1Component, data: {titulo: 'Grafica'} },
           { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
         ]
     }
